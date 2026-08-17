17c83353a3f064843731d5fab0ff82d64a736d78
jonaaix <jonaaix@icloud.com>
2026-08-17 20:21:49 +0200

    Do not enforce broadcast connection

--- src/Concerns/InteractsWithIslands.php
+++ src/Concerns/InteractsWithIslands.php
@@ -4,6 +4,8 @@ namespace Aaix\LaravelIslands\Concerns;
 
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Database\Eloquent\BroadcastsEvents;
+use Illuminate\Support\Facades\Log;
+use Throwable;
 
 trait InteractsWithIslands
 {
@@ -15,11 +17,36 @@ trait InteractsWithIslands
      */
     public function broadcastOn($event): array
     {
+        if (! $this->broadcastingIsUsable()) {
+            return [];
+        }
+
         return [new PrivateChannel($this->islandChannel())];
     }
 
     public function islandChannel(): string
     {
         return str_replace('\\', '.', static::class) . '.' . $this->getKey();
+    }
+
+    protected function broadcastIfBroadcastChannelsExistForEvent($instance, $event, $channels = null)
+    {
+        try {
+            return parent::broadcastIfBroadcastChannelsExistForEvent($instance, $event, $channels);
+        } catch (Throwable $e) {
+            Log::warning('Island broadcast skipped: ' . $e->getMessage(), [
+                'model' => static::class,
+                'event' => $event,
+            ]);
+
+            return null;
+        }
+    }
+
+    protected function broadcastingIsUsable(): bool
+    {
+        $driver = config('broadcasting.default');
+
+        return $driver !== null && $driver !== 'null';
     }
 }
