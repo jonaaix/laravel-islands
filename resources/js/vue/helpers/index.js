4f26f2e9f7bf1c90432f7cf5939033d4df8e2196
jonaaix <jonaaix@icloud.com>
2026-08-29 14:29:30 +0000

    Helpers: take in the multi select too, and let the application keep the words

--- resources/js/vue/helpers/index.js
+++ resources/js/vue/helpers/index.js
@@ -3,6 +3,7 @@ export { default as Switch } from './Switch.vue';
 export { default as Radio } from './Radio.vue';
 export { default as RadioGroup } from './RadioGroup.vue';
 export { default as Slider } from './Slider.vue';
+export { default as MultiSelect } from './MultiSelect.vue';
 export { default as OptionStrip } from './OptionStrip.vue';
 export { default as TreeSelect } from './TreeSelect.vue';
 export { nextRadioGroupName, RADIO_GROUP_KEY } from './radioGroup.js';
