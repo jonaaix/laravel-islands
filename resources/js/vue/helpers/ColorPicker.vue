a6fb6f3c4bd0c682c9c3b7ee6c7d8548eeaa4f1e
jonaaix <jonaaix@icloud.com>
2026-09-02 20:46:59 +0000

    ColorPicker: The value gets a line of its own under the format switch

--- resources/js/vue/helpers/ColorPicker.vue
+++ resources/js/vue/helpers/ColorPicker.vue
@@ -251,23 +251,25 @@ const isPreset = (value) => parseHex(value) && toHex(parseHex(value)) === toHex(
                     </Tooltip>
                 </div>
 
-                <div class="flex items-center gap-2 border-t border-gray-200 pt-3 dark:border-white/10">
-                    <OptionStrip
-                        v-if="formatOptions.length > 1"
-                        variant="segmented"
-                        size="sm"
-                        :marker="false"
-                        :model-value="activeFormat"
-                        :options="formatOptions"
-                        @update:model-value="activeFormat = $event"
-                    />
+                <div class="space-y-2 border-t border-gray-200 pt-3 dark:border-white/10">
+                    <div class="flex items-center justify-between gap-2">
+                        <OptionStrip
+                            v-if="formatOptions.length > 1"
+                            variant="segmented"
+                            size="sm"
+                            :marker="false"
+                            :model-value="activeFormat"
+                            :options="formatOptions"
+                            @update:model-value="activeFormat = $event"
+                        />
 
-                    <code class="ml-auto min-w-0 truncate font-mono text-xs text-gray-700 dark:text-gray-200">{{ display }}</code>
+                        <IconButton v-if="copyable" size="sm" :label="copied ? WORDS.copied : WORDS.copy" @click="copy">
+                            <svg v-if="!copied" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"><path d="M7 3.5A1.5 1.5 0 0 1 8.5 2h3.879a1.5 1.5 0 0 1 1.06.44l3.122 3.12A1.5 1.5 0 0 1 17 6.622V12.5a1.5 1.5 0 0 1-1.5 1.5h-1v-3.379a3 3 0 0 0-.879-2.121L10.5 5.379A3 3 0 0 0 8.379 4.5H7v-1Z"/><path d="M4.5 6A1.5 1.5 0 0 0 3 7.5v9A1.5 1.5 0 0 0 4.5 18h7a1.5 1.5 0 0 0 1.5-1.5v-5.879a1.5 1.5 0 0 0-.44-1.06L9.44 6.439A1.5 1.5 0 0 0 8.378 6H4.5Z"/></svg>
+                            <svg v-else class="h-4 w-4 text-emerald-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"><path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd"/></svg>
+                        </IconButton>
+                    </div>
 
-                    <IconButton v-if="copyable" size="sm" :label="copied ? WORDS.copied : WORDS.copy" @click="copy">
-                        <svg v-if="!copied" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"><path d="M7 3.5A1.5 1.5 0 0 1 8.5 2h3.879a1.5 1.5 0 0 1 1.06.44l3.122 3.12A1.5 1.5 0 0 1 17 6.622V12.5a1.5 1.5 0 0 1-1.5 1.5h-1v-3.379a3 3 0 0 0-.879-2.121L10.5 5.379A3 3 0 0 0 8.379 4.5H7v-1Z"/><path d="M4.5 6A1.5 1.5 0 0 0 3 7.5v9A1.5 1.5 0 0 0 4.5 18h7a1.5 1.5 0 0 0 1.5-1.5v-5.879a1.5 1.5 0 0 0-.44-1.06L9.44 6.439A1.5 1.5 0 0 0 8.378 6H4.5Z"/></svg>
-                        <svg v-else class="h-4 w-4 text-emerald-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"><path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd"/></svg>
-                    </IconButton>
+                    <code class="block truncate font-mono text-xs text-gray-700 dark:text-gray-200">{{ display }}</code>
                 </div>
             </div>
         </Popover>
