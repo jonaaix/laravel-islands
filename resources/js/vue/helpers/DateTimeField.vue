9f9dedb18ab8812774b9f5849e487777f9134a10
jonaaix <jonaaix@icloud.com>
2026-09-07 22:26:11 +0000

    DateTimeField: Today wears a gray ring

--- resources/js/vue/helpers/DateTimeField.vue
+++ resources/js/vue/helpers/DateTimeField.vue
@@ -304,7 +304,7 @@ function dayTone(cell) {
 
 function dayClass(cell) {
     if (sameDay(cell.date, draft.value)) return 'font-semibold';
-    if (sameDay(cell.date, today)) return 'ring-1 ring-inset ring-primary-400 dark:ring-primary-500/60';
+    if (sameDay(cell.date, today)) return 'ring-1 ring-inset ring-gray-300 dark:ring-white/20';
 
     return cell.inMonth ? '' : 'opacity-50';
 }
